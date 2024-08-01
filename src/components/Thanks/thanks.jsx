ff65510c66649a0c3757ee2e307695ce80c30c30
Ruan Fonseca <64973978+Ruanfonseca@users.noreply.github.com>
2024-08-01 19:47:53 -0300

    finalmente

--- src/components/Thanks/thanks.jsx
+++ src/components/Thanks/thanks.jsx
@@ -7,38 +7,37 @@ import {
 } from 'react-icons/bs';
 import './Thanks.css';
 
-
-const emojiData ={
-  insatisfeito:<BsFillEmojiFrownFill/>,
-  Um_pouco_Insatisfeito:<BsFillEmojiNeutralFill/>,
-  Muito_Satisfeito:<BsFillEmojiHeartEyesFill/>,
-  satisfeito:<BsFillEmojiSmileFill/>
+const emojiData = {
+  insatisfeito: <BsFillEmojiFrownFill />,
+  Um_pouco_Insatisfeito: <BsFillEmojiNeutralFill />,
+  Muito_Satisfeito: <BsFillEmojiHeartEyesFill />,
+  satisfeito: <BsFillEmojiSmileFill />
 }
 
-const thanks = ({data}) => {
-    return (
-      <div className='thanks-container'>
-        <h2>Falta pouco...</h2>
-        <p>Para concluir a avaliação clique no botão de Enviar abaixo.</p>
-        <br/>
-        <h3>Aqui está o resumo do sua avaliação  {data.nome}<br/></h3>
-        
-        <p className="review-data">
-        <br/>
-          <span>Satisfação com o produto</span>
-          <br/>
-          {emojiData[data.sentimento]}
-        </p>
-  
-        <p className="review-data">
-        <br/>
-          <span>Comentário:</span>
-          <br/>
-          {data.comentario}
-        </p>
-  
-      </div>
-    )
-  }
-  
-  export default thanks
+const thanks = ({ data }) => {
+  return (
+    <div className='thanks-container'>
+      <h2>Falta pouco...</h2>
+      <p>Para concluir a avaliação clique no botão de Enviar abaixo.</p>
+      <br />
+      <h3>Aqui está o resumo do sua avaliação {data.nome}<br /></h3>
+      
+      <p className="review-data">
+        <br />
+        <span>Satisfação com o produto</span>
+        <br />
+        {emojiData[data.sentimento]}
+      </p>
+
+      <p className="review-data comment-section">
+        <br />
+        <span>Comentário:</span>
+        <br />
+        {data.comentario}
+      </p>
+
+    </div>
+  )
+}
+
+export default thanks;
